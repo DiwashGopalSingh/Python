713a88dc512b5458223fba65fd10624807cb6529
Diwash Gopal Singh <diwashgs64@gmail.com>
2026-09-20 18:32:28 +0530

    New Code

---
+++ Sum of Natural Number.py
@@ -0,0 +1,7 @@
+i = 1
+sum = 1
+n = int(input("Enter a Number:"))
+while i <= n:
+    print("The sum is ", sum)
+    i += 1
+    sum = sum + i
---
+++ Sum of Whole Number.py
@@ -0,0 +1,7 @@
+i = 0
+sum = 0
+n = int(input("Enter a number: "))
+while i <= n:
+    print("The sum is ", sum)
+    i += 1
+    sum = sum + i
